7a354d7309cd409cd74d15bc023d695ade012f70
Jdelta9 <35478858+Jdelta9@users.noreply.github.com>
2018-05-06 22:58:27 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -4,3 +4,10 @@ var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var Twitter = require("twitter");
 var Spotify = require('node-spotify-api');
+
+//Request is designed to be the simplest way possible to make http calls
+request('http://www.google.com', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+});
